ed4820a49d93ea82aae60da279d465c1bcee5053
RizkyF11 <rizkyfebrian241@gmail.com>
2024-09-20 10:13:47 +0700

    membuat fitur tambah data user

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 
      <!-- Custom styles for this page -->
-     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+     <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
